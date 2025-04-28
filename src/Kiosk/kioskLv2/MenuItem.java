a4eaf038a7dd1ee6172633fd92205ab2a63184ee
gyogyo0212 <gyogyo0212@gamil.com>
2025-04-28 16:53:43 +0900

    MenuItem클래스 작성

--- src/Kiosk/kioskLv2/MenuItem.java
+++ src/Kiosk/kioskLv2/MenuItem.java
@@ -1,4 +1,32 @@
 package Kiosk.kioskLv2;
 
 public class MenuItem {
+    //속성
+    private String name;
+    private String price;
+    private String menuInfo;
+    //생성자
+    public MenuItem(String name, String price, String menuInfo){
+        this.name = name;
+        this.price = price;
+        this.menuInfo = menuInfo;
+    }
+    //기능
+
+
+    public String getName(){
+        return name;
+
+    }
+    public String getPrice(){
+        return price;
+
+    }
+    public String getMenuInfo(){
+        return menuInfo;
+    }
+
+
+
+
 }
