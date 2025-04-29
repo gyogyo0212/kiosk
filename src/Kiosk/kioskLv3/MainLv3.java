df92216a640d8872f9cacc35a6361b112773e384
gyogyo0212 <gyogyo0212@gamil.com>
2025-04-29 17:20:49 +0900

    레벨 3 코드작성 진행중

--- src/Kiosk/kioskLv3/MainLv3.java
+++ src/Kiosk/kioskLv3/MainLv3.java
@@ -1,4 +1,19 @@
 package Kiosk.kioskLv3;
 
-public class Main {
+
+import Kiosk.kioskLv2.MenuItem;
+
+import java.util.ArrayList;
+
+public class MainLv3 {
+    public static void main(String[] args) {
+        ArrayList<MenuItem> menuItem = new ArrayList<MenuItem>();
+
+        menuItem.add(new MenuItem(""));
+        menuItem.add(menuItem2);
+        menuItem.add(menuItem3);
+        menuItem.add(menuItem4);
+
+
+    }
 }
