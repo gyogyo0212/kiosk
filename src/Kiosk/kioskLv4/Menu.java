76ba2d3ef3b346855ae060ac1ba7b11592219f7c
gyogyo0212 <gyogyo0212@gamil.com>
2025-04-30 21:09:49 +0900

    Menu클래스 미완성

--- src/Kiosk/kioskLv4/Menu.java
+++ src/Kiosk/kioskLv4/Menu.java
@@ -17,10 +17,16 @@ public class Menu {
 
     public Menu(String name){
         this.name = name;
+        this.menuItem = new ArrayList<>();
+        // 메뉴
+        menuItem.add(new MenuItem("1.ShackBuger ", "W 6.9", "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
+        menuItem.add(new MenuItem("2.SmokeBuger ", "W 8.9", "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
+        menuItem.add(new MenuItem("3.CheeseBuger", "W 6.9", "포테이토 번과 비프페티, 치즈가 토핑된 치즈버거"));
+        menuItem.add(new MenuItem("4.HamBuger   ", "W 6.9", "비프패티를 기반으로 야채가 들어간 버거"));
+
     }
-    public Menu(ArrayList<MenuItem>menuItem){
-        this.menuItem= menuItem;
-    }
+
+
     public Menu(Kiosk kiosk){
         this.kiosk = kiosk;
     }
@@ -29,7 +35,9 @@ public class Menu {
     public String getName(){
         return this.name;
     }
-    public void menu(){}
+    public ArrayList<MenuItem>getMenuItem(){
+        return this.menuItem;
+    }
 
     }
 
