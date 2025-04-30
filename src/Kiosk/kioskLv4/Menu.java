de904518f2e6bce5054d1f40ca461f70b86d3b83
gyogyo0212 <gyogyo0212@gamil.com>
2025-04-30 19:50:20 +0900

    Menu클래스 작성중

--- src/Kiosk/kioskLv4/Menu.java
+++ src/Kiosk/kioskLv4/Menu.java
@@ -1,4 +1,37 @@
 package Kiosk.kioskLv4;
 
+import Kiosk.kioskLv2.MenuItem;
+import Kiosk.kioskLv3.Kiosk;
+
+import java.util.ArrayList;
+
 public class Menu {
-}
+
+    //속성
+    private String name;
+    private ArrayList<MenuItem> menuItem;
+    private Kiosk kiosk;
+
+    //생성자
+
+
+    public Menu(String name){
+        this.name = name;
+    }
+    public Menu(ArrayList<MenuItem>menuItem){
+        this.menuItem= menuItem;
+    }
+    public Menu(Kiosk kiosk){
+        this.kiosk = kiosk;
+    }
+
+    //기능
+    public String getName(){
+        return this.name;
+    }
+    public void menu(){}
+
+    }
+
+
+
